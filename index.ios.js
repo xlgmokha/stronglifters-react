7c8abbb38daab521d551fc2fd2b740944806204e
mo khan <mo@mokhan.ca>
2016-11-26 21:27:58 -0700

    remove stylesheet

--- index.ios.js
+++ index.ios.js
@@ -2,13 +2,4 @@ import React, { Component } from 'react';
 import { AppRegistry, StyleSheet } from 'react-native';
 import ApplicationShell from './app/components/application-shell'
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-});
-
 AppRegistry.registerComponent('AwesomeProject', () => ApplicationShell);
