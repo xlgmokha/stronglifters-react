b786add70097bd4c7ac96f58fe5a827f8d8e4b81
mo khan <mo@mokhan.ca>
2016-12-25 12:13:45 -0700

    capture click on exercise.

--- app/screens/new-workout-screen.js
+++ app/screens/new-workout-screen.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Container, Header, Title, Content, Footer, FooterTab, Button, Icon, Spinner, DeckSwiper, Text } from 'native-base';
+import { Container, Header, Title, Content, Footer, FooterTab, Button, Icon, Spinner, DeckSwiper, Text, List, ListItem } from 'native-base';
 import Screen from './screen';
 import * as events from '../services/events';
 
@@ -31,10 +31,19 @@ export default class NewWorkoutScreen extends Screen {
           </Header>
           <Content>
             <Text>Body Weight: {this.state.body_weight.amount} {this.state.body_weight.unit}</Text>
+            <List
+              dataArray={this.state.exercises}
+              renderRow={(exercise) => <ListItem button onPress={() => this.onPress(exercise)}><Text>{exercise.name}</Text></ListItem>}
+              >
+            </List>
           </Content>
         </Container>
       );
     }
+  }
+
+  onPress(exercise) {
+    console.log(`pressed ${exercise.name}`)
   }
 
   onLoadWorkout() {
