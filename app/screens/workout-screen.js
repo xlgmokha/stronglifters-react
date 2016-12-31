22cdcc870306cb780d8c31626d5d04ade021de99
mo khan <mo@mokhan.ca>
2016-12-31 15:32:20 -0700

    render each exercise on the workout screen.

--- app/screens/workout-screen.js
+++ app/screens/workout-screen.js
@@ -1,5 +1,6 @@
 import * as events from '../services/events';
 import React, { Component } from 'react';
+import { View } from 'react-native';
 import Screen from './screen';
 import { Container, Header, Title, Content, Footer, FooterTab, Button, Icon, Spinner, DeckSwiper, Text, List, ListItem } from 'native-base';
 
@@ -11,15 +12,21 @@ export default class WorkoutScreen extends Screen {
           <Title>Routine {this.props.routine.name}</Title>
         </Header>
         <Content>
-          <List dataArray={this.props.sets} renderRow={this.renderSet.bind(this)}></List>
+          <List dataArray={this.props.exercises} renderRow={this.renderExercise.bind(this)}></List>
         </Content>
       </Container>
     );
   }
 
-  renderSet(set) {
+  renderExercise(exercise) {
+    const sets = this.props.sets.filter((set) => set.exercise_id == exercise.id);
     return (
-      <Text>hello</Text>
+      <ListItem button>
+        <View style={{flex: 1}}>
+          <Text>{exercise.name}</Text>
+          {sets.map((set) => <Button key={set.id} rounded info>{set.actual_repetitions || ' '}</Button>)}
+        </View>
+      </ListItem>
     );
   }
 }
